0e45f264133c20bfc82cf91a7a50f5b0298a8f2c
Yorick <yorick.estievenart@gmail.com>
2021-06-14 14:21:39 +0200

    first commit

---
+++ src/main/java/view/FXLauncher.java
@@ -0,0 +1,23 @@
+package view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class FXLauncher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Minesweeper");
+        primaryStage.setWidth(1200);
+        primaryStage.setHeight(700);
+        primaryStage.setScene(new Scene(new Pane()));
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
